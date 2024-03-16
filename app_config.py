7b2a6ed18d096911562c570afd39857b9b67e1de
krystian <krystian904@gmail.com>
2024-03-16 20:57:48 +0100

    dodano poprawki z Pull request -> config module #1

--- app_config.py
+++ app_config.py
@@ -5,30 +5,38 @@ from datetime import datetime
 
 class AppConfig:
     
+    DEFAULT_CONFIG_PATH = "config\\defaultAppConfig.json"
+    SAVE_CONFIG_PATH = "tmp\\appConfig.json"
+    MKDIR_PATH = os.getcwd()
+
     def __init__(self):
-        with open('defultConfig.json') as f:
-            self.defluat = json.load(f)
-            self.location_config = self.defluat['location_config'] + "\\sample.json"
+        if os.path.exists('./tmp'):
+            print("sciezka istnieje")
+        else :
+                  
+            os.system('mkdir tmp')
+             
+        
+        if os.path.exists(os.path.join(AppConfig.MKDIR_PATH , AppConfig.SAVE_CONFIG_PATH)) ==False:
+            print("plik app config nie istnieje")
+            print(os.path.join(AppConfig.MKDIR_PATH ,AppConfig.DEFAULT_CONFIG_PATH))
+            with open(os.path.join(AppConfig.MKDIR_PATH ,AppConfig.DEFAULT_CONFIG_PATH)) as readFile:
+                  self.default = json.load(readFile)
+            with open(os.path.join(AppConfig.MKDIR_PATH , AppConfig.SAVE_CONFIG_PATH), "w") as writeFile:
+                 json.dump(self.default, writeFile)       
 
-        if os.path.isfile(self.location_config):
-                with open(self.location_config) as file_read:
-                     self.config = json.load(file_read)
-        else:
-                tmpConfig = self.defluat
-                tmpConfig.pop("location_config")
-                now = datetime.now()
-                tmpConfig['timeToCreate'] = str(now.strftime("%Y:%m:%d  %H:%M:%S"))
+        
 
-                with open(self.location_config, "w") as outfile:
-                    json.dump(tmpConfig, outfile)
+        with open(os.path.join(AppConfig.MKDIR_PATH ,AppConfig.SAVE_CONFIG_PATH)) as readFile:
+             self.config = json.load(readFile)
+             print(self.config)
 
-                with open(self.location_config) as file_read:
-                     self.config = json.load(file_read)        
-          
 
-    def save_config(self):
-        with open(self.location_config , "w") as outfile:
-             json.dump(self.config, outfile)
+             
+    def saveConfig(self):
+        with open(os.path.join(AppConfig.MKDIR_PATH , AppConfig.SAVE_CONFIG_PATH), "w") as writeFile:
+                 json.dump(self.config, writeFile)
+
 
     def get_param(self , inputParametr ):
         return(self.config[inputParametr])
@@ -36,13 +44,24 @@ class AppConfig:
     def set_param(self , inputParametr , value):
         tmp_paramet = {inputParametr , value}
         self.config[inputParametr] = value
-        self.save_config()
-        #print("run set_param inputParmetr - ", inputParametr , "  value - " , value )
+        self.saveConfig()
+        
 
     def get_location_config(self):
-        return self.location_config
+        return AppConfig.MKDIR_PATH
     
+    def __str__(self):
+        return str(self.config)
     
+
+test = AppConfig()
+print(test.get_param("database_adres"))
+test.set_param("database_adres", "127.1.1.0")
+
+if __name__ == "__main__":
+    print("Class app_config run here")
+    
+
 
 
 
--- tryApp_config.py
+++
@@ -1,7 +0,0 @@
-import app_config
-
-app = app_config.AppConfig()
-print(app.get_location_config())
-print(app.get_param("port"))
-app.set_param("port","777")
-print(app.get_param("port"))
